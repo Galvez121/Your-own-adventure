17dcc77007d67121acbb9a0adbf2dd2489cc4595
Juan Galvez <juanjosegalvez2014@gmail.com>
2021-08-23 17:13:18 -0500

    add weather, skills, health(continue work with this)

--- my_own_adventure.py
+++ my_own_adventure.py
@@ -1,7 +1,13 @@
 #this is my adventure. Select the options and enjoy
 #The module NPC contains the npc of this game
 import npcs
+import random
 
+#Weather and environment
+
+environment = ['windy','sunny','ranny','cloudy']
+
+#Make you player
 player = input("What is your adventorous name? ")
 
 #List of habilities
@@ -9,6 +15,9 @@ elements_control = []
 
 #List of my stuff(inventory)
 weapons = []
+spells = {}
+skills = {}
+health = [1,2,3,4,5]
 
 print('Hi Adventorous',player, 'and welcome to this history, your mision is become The warrior king')
 
@@ -18,11 +27,15 @@ answer_of_the_element = input('You have to select one of this two elements contr
 while True:
     if answer_of_the_element == 'fire':
         elements_control.append('fire')
+        spells['fire ball'] = 600 #this is a spell for the magic book, the number is the power
+        skills['fire cut'] = 400
         print("You select fire control.")
         break
 
     elif answer_of_the_element == 'water':
         elements_control.append('water')
+        spells['water ball'] = 500 #this is a spell for the magic book, the number is the power
+        skills['water movement'] = 600
         print("You select water control.")
         break
     
@@ -52,4 +65,18 @@ while True:
         quit()
         
 print('Your profile is done.\nYour element:',elements_control[0],'\nYour weapon:',weapons[0])
+print()
+
+#Here start the history
+print('Welcome new adventorous this is your first day in this world\nGo to the tavern and select your task')
+print()
+
+
+
+
+
+#here is the radom weather (Work with this later)
+random_election = random.choice(environment)
+
+print(random_election)
 print('The end')
--- npcs.py
+++ npcs.py
@@ -1,3 +1,5 @@
 def lopez():
-    name = 'lopez'
+    name = 'Lopez'
     return name
+
+#Make a class with the name "bartender"
