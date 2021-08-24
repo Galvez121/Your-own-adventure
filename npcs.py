81e5c3aa5a93197d09de2f16280408f9eedba904
Juan Galvez <juanjosegalvez2014@gmail.com>
2021-08-23 23:29:44 -0500

    make the class npcs, I wil use it for the persons in the game

--- npcs.py
+++ npcs.py
@@ -1,5 +1,9 @@
-def lopez():
-    name = 'Lopez'
-    return name
 
-#Make a class with the name "bartender"
+#Class Npc contains all persons of the games
+
+class Npcs():
+    def npc_lopez(self):
+        return 'Hi my name is Lopez'
+    
+    def npc_bartender(self): #continue later this her
+        return None
