f58268680df8fcbf1777293aec158e12cf3315c6
Juan Galvez <juanjosegalvez2014@gmail.com>
2021-08-24 23:29:23 -0500

    I made the code more readable and I made the forest way

--- my_own_adventure.py
+++ my_own_adventure.py
@@ -1,94 +1,147 @@
-#this is my adventure. Select the options and enjoy
-#The module NPC contains the npc of this game
-from npcs import Npcs
+# Tis is my adventure. Select the options and enjoy
+from npcs import Npcs  # The module NPC contains the npc of this game
 import random
 
-#Weather and environment
+# Weather and envioronment
 
-environment = ['windy','sunny','ranny','cloudy']
+ENVIROMENT = ("windy", "sunny", "rainy", "cloudy")
 
-#Make you player
+# Make you player
 player = input("What is your adventorous name? ")
 
-#List of habilities
+# ist of habilities
 elements_control = []
 
-#List of my stuff(inventory)
+# List of my stuff(inventory)
 weapons = []
 spells = {}
 skills = {}
 health = 4
 
-#enemies
-enemis = ['goblins', 'big bats','big mouses']
+# Enemies
+enemis = ["goblin", "big bat", "big mouse"]
 
-print('Hi Adventorous',player, 'and welcome to this history, your mision is become The warrior king')
+print(
+    "Hi Adventorous",
+    player,
+    "and welcome to this history, your mision is become The warrior king",
+)
 
-answer_of_the_element = input('You have to select one of this two elements control, fire or water. What of those do you want to control? (fire/water) ').lower()
+answer_of_the_element = input(
+    "You have to select one of this two elements control, fire or water. What of those do you want to control? (fire/water) "
+).lower()
 
-#Here you select your element
+# Here you select your element
 while True:
-    if answer_of_the_element == 'fire':
-        elements_control.append('fire')
-        spells['fire ball'] = 600 #this is a spell for the magic book, the number is the power
-        skills['fire cut'] = 400
+    if answer_of_the_element == "fire":
+        elements_control.append("fire")
+        spells[
+            "fire ball"
+        ] = 600  # This is a spell for the magic book, the number is the power
+        skills["fire cut"] = 400
         print("You select fire control.")
         break
 
-    elif answer_of_the_element == 'water':
-        elements_control.append('water')
-        spells['water ball'] = 500 #this is a spell for the magic book, the number is the power
-        skills['water movement'] = 600
+    elif answer_of_the_element == "water":
+        elements_control.append("water")
+        spells[
+            "water ball"
+        ] = 500  # This is a spell for the magic book, the number is the power
+        skills["water movement"] = 600
         print("You select water control.")
         break
-    
+
     else:
-        print('You have to select fire or water. This option is wrong. You lose')
+        print("You have to select fire or water. This option is wrong. You lose")
         quit()
-            
-#Here you select your weapon
-#The class NPC contains the npcs of this game
+
+# Here you select your weapon
+# The class NPC contains the npcs of this game
 
 dialogue = Npcs()
 
-print(dialogue.npc_lopez()) 
+print(dialogue.npc_lopez())
 while True:
-    answer_of_the_weapon = input('Each new warrior needs to select one of these weapons magic sword or spell book. Select one \nwrite sword or book ').lower()
+    answer_of_the_weapon = input(
+        "Each new warrior needs to select one of these weapons magic sword or spell book. Select one \nwrite sword or book "
+    ).lower()
 
-    if answer_of_the_weapon == 'book':
-        weapons.append('spell book')
+    if answer_of_the_weapon == "book":
+        weapons.append("spell book")
         print("You select a spell book.")
         print()
         break
-    elif answer_of_the_weapon == 'sword':
-        weapons.append('magic sword')
+    elif answer_of_the_weapon == "sword":
+        weapons.append("magic sword")
         print("You select a magic sword.")
         print()
         break
     else:
-        print('You have to select book or sword. This option is wrong. You lose')
+        print("You have to select book or sword. This option is wrong. You lose")
         quit()
-        
-print('Your profile is done.\nYour element:',elements_control[0],'\nYour weapon:',weapons[0])
+
+print(
+    "Your profile is done.\nYour element:",
+    elements_control[0],
+    "\nYour weapon:",
+    weapons[0],
+)
 print()
 
-#Here start the history
-print('Welcome new adventorous this is your first day in this world\nGo to the tavern and select your task')
+# Here start the history
+print(
+    "Welcome new adventorous this is your first day in this world\nGo to the tavern and select your task"
+)
 print()
 
 
-#Forest
-'''while health >= 0:
-    print('You enter the forest and find a men, do you want to talk with him or do you want to conitinue your way')
-    election = input('Write talk or continuo ').lower()
+# Forest
+while health >= 0:
+    print(
+        "You enter the forest and you find a old men, do you want to talk with him or do you want to continue your way"
+    )
+    election = input("Write talk or continue ").lower()
+    print()
     random_election = random.choice(enemis)
-    if election == 'talk':
-        print('You continue and then you lose yourself and die for a',random_election)
-        health -= 1'''
+    if election == "continue":
+        print(
+            "You continue and then you lose yourself and you are hurt by a",
+            random_election,
+        )
+        health -= 1
+        print("You come back and you decide to talk with the old men")
+        print()
+        print(dialogue.npc_old_men())
+        your_choice = input(
+            "Do you want to go to the rouins or do you want to continue\nWrite go or continue "
+        ).lower()
+        print()
+        if your_choice == "continue":
+            print(
+                "You continue and then you lose yourself and you are hurt by a",
+                random_election,
+            )
+            health -= 1
+            print("You are so dumb, Please go to the rouins")
+            print()
+            your_choice = input(
+                "Do you want to go to the rouins or do you want to continue\nWrite go or continue "
+            ).lower()
+            if your_choice == "continue":
+                print("You die for a", random_election)
+                health -= 3
+    elif election == "talk":
+        print(dialogue.npc_old_men())
 
- 
-#here is the radom weather (Work with this later)
-random_election = random.choice(environment)
+    else:
+        health -= 4
+
+if health <= 0:
+    print("You lost")
+
+
+# Here is the radom weather (Work with this later)
+random_election = random.choice(ENVIROMENT)
 
 print(random_election)
-print('The end')
+print("The end")
--- npcs.py
+++ npcs.py
@@ -1,9 +1,12 @@
+"""Class Npc contains all persons of the games"""
 
-#Class Npc contains all persons of the games
 
-class Npcs():
+class Npcs:
     def npc_lopez(self):
-        return 'Hi my name is Lopez'
-    
-    def npc_bartender(self): #continue later this her
+        return "Hi my name is Lopez"
+
+    def npc_bartender(self):  # continue later with her
         return None
+
+    def npc_old_men(self):
+        return " - Hi adventorous, take this mape and go to that ruins. Good luck and I hope that you come back alive"
